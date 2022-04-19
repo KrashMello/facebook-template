4491e0186cb34ec988c5822185d2566d4f847037
smoke <joelenrikesoteldo@gmail.com>
2022-04-19 08:24:47 -0400

    initial commit

---
+++ components/postCard.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-card class="mt-5">
+    <v-container>
+      <v-row align="center">
+        <v-col class="py-0 mr-3" style="max-width: 50px; width: 50px" cols="2">
+          <v-avatar size="50">
+            <v-img
+              alt="user"
+              src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+            />
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0 px-0" cols="8">
+          <v-card-title class="body-1 font-weight-bold">Nike</v-card-title>
+          <v-card-subtitle
+            >30 min <v-icon small>mdi-earth</v-icon></v-card-subtitle
+          >
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <p class="body-2 text-justify" style="height: 50px; max-height: 50px">
+            {{ truncatePostContent }}
+          </p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="px-0">
+          <v-card flat width="600">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${3 * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${3 * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            ></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row justify="space-between">
+        <v-col cols="2">
+          <p class="grey-text text--lighten-3">
+            <v-icon color="primary">mdi-thumb-up</v-icon>
+            1k
+          </p>
+        </v-col>
+        <v-col cols="5">
+          <p class="grey-text text--lighten-3">150 comments 200 shares</p>
+        </v-col>
+      </v-row>
+      <v-divider />
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-btn large elevation="0" text color="grey">
+            <v-icon>mdi-thumb-up</v-icon>
+            Like
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn large elevation="0" text color="grey">
+            <v-icon>mdi-comment-outline</v-icon>
+            Comment
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn large elevation="0" text color="grey">
+            <v-icon>mdi-share-outline</v-icon>
+            share
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-divider />
+      <v-row aling="center">
+        <v-col cols="3">
+          <v-btn color="primary" text>View Comment</v-btn>
+        </v-col>
+      </v-row>
+      <v-row aling="center">
+        <v-col class="pb-0 mb-0" cols="12">
+          <v-text-field
+            dense
+            filled
+            rounded
+            background-color="grey lighten-2"
+            placeholder="Write a comment..."
+          >
+            <template #prepend>
+              <v-avatar size="45">
+                <v-img
+                  alt="user"
+                  src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+                />
+              </v-avatar>
+            </template>
+            <template #append>
+              <v-btn icon>
+                <v-icon>mdi-emoticon-outline</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-camera-outline</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-sticker-emoji</v-icon>
+              </v-btn>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      postContent:
+        'Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.',
+    }
+  },
+  computed: {
+    truncatePostContent() {
+      if (this.postContent.length > 300) {
+        return this.postContent.substring(0, 300) + '...'
+      } else {
+        return this.postContent
+      }
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,214 @@
+<template>
+  <v-container fluid class="pt-0 grey lighten-3">
+    <v-row justify="space-around">
+      <v-col cols="10">
+        <v-card class="d-flex">
+          <v-img
+            height="400px"
+            class="align-end"
+            src="https://picsum.photos/id/11/500/300"
+          >
+            <v-card flat class="rounded-0">
+              <v-card-title class="align-end">
+                <v-avatar
+                  style="position: absolute; border: solid 2px #ffffff"
+                  size="80"
+                >
+                  <img
+                    alt="user"
+                    src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+                  />
+                </v-avatar>
+                <p style="margin-left: 100px">John Doe</p>
+                <v-spacer />
+                <v-btn small color="primary">
+                  <v-icon small>mdi-pencil</v-icon> contact us
+                </v-btn>
+              </v-card-title>
+              <v-divider />
+              <v-toolbar dense max-height="50px">
+                <v-tabs v-model="tab" align-with-title>
+                  <v-tabs-slider color="primary"></v-tabs-slider>
+                  <v-tab v-for="item in items" :key="item">
+                    {{ item }}
+                  </v-tab>
+                </v-tabs>
+                <v-spacer />
+                <v-btn elevation="0" small color="grey lighten-3" class="mr-3">
+                  <v-icon small class="mr-3"> mdi-thumb-up </v-icon>
+                  Like
+                </v-btn>
+                <v-btn elevation="0" small color="grey lighten-3" class="mr-3">
+                  <v-icon small class="mr-3"> mdi-facebook-messenger </v-icon>
+                  Messenger
+                </v-btn>
+                <v-btn elevation="0" small color="grey lighten-3" class="mr-3">
+                  <v-icon small> mdi-magnify </v-icon>
+                </v-btn>
+                <v-btn elevation="0" color="grey lighten-3" small> ... </v-btn>
+              </v-toolbar>
+            </v-card>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="mt-7" justify="center" align="center">
+      <v-col cols="10">
+        <v-card flat color="grey lighten-3">
+          <v-container fluid>
+            <v-row>
+              <v-col md="5">
+                <v-card>
+                  <v-card-title>About</v-card-title>
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-icon>
+                        <v-icon>mdi-information</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          Lorem ipsum dolor sit amet consectetur adipiscing
+                          elit, urna consequat felis vehicula class ultricies
+                          mollis dictumst, aenean non a in donec nulla.
+                          Phasellus ante pellentesque erat cum risus consequat
+                          imperdiet aliquam, integer placerat et turpis mi eros
+                          nec lobortis taciti, vehicula nisl litora tellus
+                          ligula porttitor metus.
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon>
+                        <v-icon>mdi-thumb-up</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          150.202.220 People like this
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon>
+                        <v-icon>mdi-briefcase-check</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          143.555.200 People follow this
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon>
+                        <v-icon>mdi-web</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          <v-btn small text color="primary"
+                            >https://helloworld.com/</v-btn
+                          >
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-icon>
+                        <v-icon>mdi-facebook-messenger</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          <v-btn text color="primary">Send Message</v-btn>
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-card>
+                <v-card class="mt-5">
+                  <v-card-title
+                    >Photos
+                    <v-spacer />
+                    <v-btn text small color="primary">See all</v-btn>
+                  </v-card-title>
+                  <v-card flat>
+                    <v-container fluid>
+                      <v-row justify="center" class="mb-3">
+                        <v-col
+                          v-for="n in 9"
+                          :key="n"
+                          class="d-flex child-flex pa-1"
+                          cols="4"
+                        >
+                          <v-img
+                            :src="`https://picsum.photos/500/300?image=${
+                              n * 5 + 10
+                            }`"
+                            :lazy-src="`https://picsum.photos/10/6?image=${
+                              n * 5 + 10
+                            }`"
+                            aspect-ratio="1"
+                            class="grey lighten-2"
+                          ></v-img>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card>
+                </v-card>
+              </v-col>
+              <v-col md="7">
+                <v-card class="d-flex">
+                  <v-container>
+                    <v-row justify="space-between" align="center">
+                      <v-col cols="2">
+                        <v-avatar size="50">
+                          <img
+                            alt="user"
+                            src="https://cdn.pixabay.com/photo/2020/06/24/19/12/cabbage-5337431_1280.jpg"
+                          />
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="10">
+                        <v-btn block color="grey lighten-1" elevation="0"
+                          >Create Post</v-btn
+                        >
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center">
+                      <v-btn elevation="0" large color="white">
+                        <v-icon color="green" class="mr-2"
+                          >mdi-image-multiple</v-icon
+                        >
+                        Photo/Video
+                      </v-btn>
+                      <v-btn elevation="0" large color="white">
+                        <v-icon color="red" class="mr-2">mdi-map-marker</v-icon>
+                        Check in
+                      </v-btn>
+                      <v-btn elevation="0" large color="white">
+                        <v-icon color="primary" class="mr-2"
+                          >mdi-account-plus</v-icon
+                        >
+                        Tag Friend
+                      </v-btn>
+                    </v-row>
+                  </v-container>
+                </v-card>
+                <!-- agregar el PostCard -->
+                <postCard v-for="n in 2" :key="n" />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      tab: null,
+      items: ['Home', 'About', 'Photos', 'Instagram'],
+    }
+  },
+}
+</script>
